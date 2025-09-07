145dcb205040e10f155571f6b323e5c9e925777e
Matias Di Lella <matias.adilella@gmail.com>
2025-09-07 19:31:00 -0300

    Create transform_data.py
    Se implementa la clase para transformar los datos (normalizamos).

---
+++ transform_data.py
@@ -0,0 +1,40 @@
+import re
+
+class DataTransformer:
+    def __init__(self):
+        
+        pass
+    
+    @staticmethod    
+    def normalize_data(nombre):
+        if not isinstance(nombre, str):
+            # El método isinstance(obj, tipo) verifica si el objeto obj es del tipo especificado.
+            nombre = ""
+        string_normalizado = re.sub(r'\[.*?\]|[\u200b]', '', nombre).strip()
+        return string_normalizado
+    
+    @staticmethod
+    def normalize_date(fecha_str):
+        MESES = {
+            "enero": "01",
+            "febrero": "02",
+            "marzo": "03",
+            "abril": "04",
+            "mayo": "05",
+            "junio": "06",
+            "julio": "07",
+            "agosto": "08",
+            "septiembre": "09",
+            "octubre": "10",
+            "noviembre": "11",
+            "diciembre": "12"
+        }
+
+        partes = fecha_str.split(" de ")
+        if len(partes) == 3:
+            dia, mes, anio = partes
+            mes_num = MESES.get(mes.lower())
+            if mes_num:
+                return f"{anio}-{mes_num}-{dia.zfill(2)}"
+        return None
+

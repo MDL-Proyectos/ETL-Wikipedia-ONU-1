0afec8a9de10733f32c1975f716cc2856261d4b9
Matias Di Lella <matias.adilella@gmail.com>
2025-09-07 19:30:01 -0300

    Update de la extracción
    Se mejora la estructura y los métodos de la clase de extract data.

--- extract_data.py
+++ extract_data.py
@@ -2,24 +2,27 @@ import json
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
+def extract():
 
-# Cargar y leer el archivo generado a través del proyecto WebScraping--1
-with open(os.getenv("DATA_JSON_PATH"), 'r', encoding='utf-8') as f:
-    datos = json.load(f)
+    load_dotenv()
 
-# Obtenemos las claves del primer diccionario en la lista
-print(datos[0].keys())
+    # Cargar y leer el archivo generado a través del proyecto WebScraping--1
+    with open(os.getenv("DATA_JSON_PATH"), 'r', encoding='utf-8') as f:
+        datos = json.load(f)
+
+    # Obtenemos las claves del primer diccionario en la lista
+   # print(datos[0].keys())
 
 
-#print(datos[0]["Denominación UNTERM[23]\u200b[i]\u200b"])
+    #print(datos[0]["Denominación UNTERM[23]\u200b[i]\u200b"])
 
-for dato in datos:
-    print(dato["Estado miembro[13]\u200b"])  # País
-    print(dato["Denominación UNTERM[23]\u200b[i]\u200b"])  # Denominación
-    print(dato["Fecha de admisión[13]\u200b"])  # Fecha de admisión
-    print(dato["Notas"])  # Notas
-    print("-----")
-    # Con el método pop() corregimos el nombre de la clave. Normalizamos el nombre de la clave.
-    dato["fecha"] = dato.pop("Fecha de admisión[13]\u200b")
-    print(dato["fecha"])
+    for dato in datos:
+        # Con el método pop() corregimos el nombre de la clave. Normalizamos el nombre de la clave.
+        dato["Estado miembro"] = dato.pop("Estado miembro[13]\u200b")
+        dato["Denominación UNTERM"] = dato.pop("Denominación UNTERM[23]\u200b[i]\u200b")
+        dato["Fecha de admisión"] = dato.pop("Fecha de admisión[13]\u200b")
+        #print(dato["Fecha de admisión"])
+
+
+        
+    return datos
